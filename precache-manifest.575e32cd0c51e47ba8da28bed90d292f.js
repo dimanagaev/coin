d842fd80200479bbba9f2ff33f21d3162b5415b3
dimanagaev <nagaev.10@bk.ru>
2020-01-12 03:19:10 +0300

    Updates

---
+++ precache-manifest.575e32cd0c51e47ba8da28bed90d292f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3c1dfa67570f053f8d715e412a75afc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5eca97594104cdf94515",
+    "url": "/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "d6ee3c5255df43da2847",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "5eca97594104cdf94515",
+    "url": "/static/js/2.bedc648f.chunk.js"
+  },
+  {
+    "revision": "d6ee3c5255df43da2847",
+    "url": "/static/js/main.25878791.chunk.js"
+  },
+  {
+    "revision": "5a7ef82d7a950dbbc98b",
+    "url": "/static/js/runtime-main.fd27ff8b.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
